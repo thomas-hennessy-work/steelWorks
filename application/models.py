303fe820e143aa1038ca5f7fb79d40154facfa43
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-10 10:01:10 +0000

    db creation working

--- application/models.py
+++ application/models.py
@@ -8,11 +8,11 @@ class Song(db.Model):
     song_group = db.Column(db.String(127), nullable=False)
     song_length = db.Column(db.Integer, nullable=False)
     song_yt_link = db.Column(db.String(511))
-    song_review = db.relationship('Review', backref='Song_review')
+    song_review = db.relationship('Review', backref='The_review')
 
 class Review(db.Model):
     review_id = db.Column(db.Integer, primary_key=True)
-    song_id = db.Column(db.Integer, db.ForeignKey('Song.song_id'), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey('song.song_id'), nullable=False)
     review_text = db.Column(db.String(1023), nullable=False)
     review_score_total = db.Column(db.Integer)
     review_mosh = db.Column(db.Integer)
