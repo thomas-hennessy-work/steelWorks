38a0a64dd9e2856ad4dd8f7fa76a2c96ea4265db
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-10 20:54:04 +0000

    added forms, implementing them to page

---
+++ application/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms import DataRequired
+
+from application.models import Song, Review
+
+#Add data required to all feilds that cannot be null in the database
+class SongForm(FlaskForm):
+    title = StringField('Song title',
+        validators=[DataRequired()])
+    group = StringField('Group'),
+        validators=[DataRequired()])
+    length = IntegerField('Song length (s)')
+        validators=[DataRequired()])
+    youTube = StringField('URL to song on YouTube')
+    submit = SubmitField('Add song')
+
+class ReviewForm(FlaskForm):
+    review_text = StringField('Review'),
+        validators=[DataRequired()])
+    score_total = IntegerField('Total score')
+    vocals = IntegerField('Vocals score')
+    riff = IntegerField('Riff score')
+    bass = IntegerField('Bass score')
+    beat = IntegerField('Beat score')
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,26 @@
-from flask import render_template
+from flask import render_template, url_for
 
-from application import app
+#importing the app and the linked database created in the initialisation file
+from application import app, db
+#Importing the database models
+from application.models import Song, Review
+#Importing the forms
+from application.forms import SongForm, ReviewForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add-song' methods=['GET', 'POST'])
+def addSong():
+    form = SongForm()
+    #if the form validates on clicking the submit button
+    if form.validate_on_submit():
+        new_song = Song(song_title=form.title.data,
+                    song_group=form.group.data,
+                    song_length=form.length.data,
+                    song_yt_link=form.youTube.data)
+        db.session.add(new_song)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add-song', form=form)
