ddf9d8bdaacdf9ce5991145c192229aaa208cf9a
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2020-11-12 19:27:46 +0000

    specified the test file, created setup and tear down

--- tests/test_songs.py
+++ tests/test_songs.py
@@ -3,6 +3,7 @@ from flask_testing import TestCase
 
 from app import app, db
 from application.routes import index
+from application.models import Song
 
 class TestBase(TestCase):
     def create_app(self):
@@ -13,5 +14,17 @@ class TestBase(TestCase):
                 )
         return app
 
+    #Used before every test to set up a test song
     def setUp(self):
+        db.create_all()
 
+        sampleSong = Song(title="Pisces", 
+                group="Jinjer",
+                length=305,
+                yt_link="https://www.youtube.com/embed/SQNtGoM3FVU")
+
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
